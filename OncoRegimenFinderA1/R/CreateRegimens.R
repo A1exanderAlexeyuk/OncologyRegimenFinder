90b587ee3c3b41fc8089c0d31ae97ab944722de4
alexander alexeuyk <87428750+A1exanderAlexeyuk@users.noreply.github.com>
2021-10-13 15:45:04 +0300

    initial commit

---
+++ OncoRegimenFinderA1/R/CreateRegimens.R
@@ -0,0 +1,132 @@
+# Copyright 2021 Observational Health Data Sciences and Informatics
+#
+# This file is part of OncoRegimenFinder
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#' Create treatment regimens of a cohort
+#'
+#' @description
+#' Computes features using all drugs, conditions, procedures, etc. observed on or prior to the cohort
+#' index date.
+#'
+#' @template Connection
+#'
+#' @template CdmDatabaseSchema
+#'
+#' @template writeDatabaseSchema
+#'
+#' @template CohortTable
+#'
+#' @param drugClassificationIdInput
+#'
+#'
+#' @param dateLagInput
+#' .
+#'
+#'@param regimenRepeats
+#'
+#'
+#'@param generateVocabTable
+#'
+#'
+#' @return
+#' A data frame drug regimens.
+#'
+#' @export
+
+createRegimens <- function(connectionDetails,
+                            cdmDatabaseSchema,
+                            writeDatabaseSchema,
+                            cohortTable = cohortTable,
+                            regimenTable = regimenTable,
+                            regimenIngredientTable = regimenIngredientTable,
+                            vocabularyTable = vocabularyTable,
+                            drugClassificationIdInput = 21601387,
+                            dateLagInput,
+                            regimenRepeats = 5,
+                            generateVocabTable = TRUE,
+                            sampleSize = 999999999999) {
+
+  connection <-  DatabaseConnector::connect(connectionDetails)
+
+  sql <- SqlRender::render(sql = getCohortBuild(),
+                           cdmDatabaseSchema = cdmDatabaseSchema,
+                           writeDatabaseSchema = writeDatabaseSchema,
+                           cohortTable = cohortTable,
+                           regimenTable = regimenTable,
+                           drugClassificationIdInput = drugClassificationIdInput)
+
+  DatabaseConnector::executeSql(connection = connection, sql = sql)
+
+  sqlTemp <- SqlRender::render("SELECT max(rn) FROM @writeDatabaseSchema.@regimenTable;", writeDatabaseSchema = writeDatabaseSchema, regimenTable = regimenTable)
+  maxId <- DatabaseConnector::dbGetQuery(connection, sqlTemp)
+  message(paste0("Cohort contains ", maxId$max, " subjects"))
+  idGroups <- c(seq(1, maxId$max, sampleSize), maxId$max + 1)
+
+  sql <- SqlRender::render(getRegimenTable_f(),
+                          regimenTable_f = paste0(regimenTable,"_f"),
+                          writeDatabaseSchema = writeDatabaseSchema)
+
+  DatabaseConnector::executeSql(connection = connection, sql = sql)
+  for(g in c(1:(length(idGroups)-1))){
+
+    startId = idGroups[g]
+    endId = idGroups[g+1] - 1
+
+    message(paste0("Processing persons ",startId," to ",endId))
+    sql <- SqlRender::render(getInsertIntoSampledRegimenTable(),
+                             writeDatabaseSchema = writeDatabaseSchema,
+                             regimenTable = regimenTable,
+                             sampledRegimenTable = paste0(regimenTable,"_sampled"),
+                             start = startId,
+                             end = endId)
+
+    DatabaseConnector::executeSql(connection = connection, sql = sql)
+
+  sql <- SqlRender::render(sql = getRegimenCalculation(),
+                           writeDatabaseSchema = writeDatabaseSchema,
+                           regimenTable = regimenTable,
+                           dateLagInput= dateLagInput)
+
+  for(i in c(1:regimenRepeats)){DatabaseConnector::executeSql(connection = connection, sql = sql)}
+
+  sql <- SqlRender::render(getInsertIntoRegimenTable_f(),
+                          writeDatabaseSchema = writeDatabaseSchema,
+                          sampledRegimenTable = paste0(regimenTable,"_sampled"),
+                          regimenTable_f = paste0(regimenTable,"_f"))
+
+  DatabaseConnector::executeSql(connection = connection, sql)
+
+  }
+  if(generateVocabTable){
+
+    sql <- SqlRender::render(sql = getRegimenVocabulary(),
+                             writeDatabaseSchema = writeDatabaseSchema,
+                             cdmDatabaseSchema = cdmDatabaseSchema,
+                             vocabularyTable = vocabularyTable)
+
+    DatabaseConnector::executeSql(connection = connection, sql = sql)
+
+  }
+
+  sql <- SqlRender::render(getRegimenFormat(),
+                           writeDatabaseSchema = writeDatabaseSchema,
+                           cohortTable = cohortTable,
+                           regimenTable = regimenTable,
+                           regimenIngredientTable = regimenIngredientTable,
+                           vocabularyTable = vocabularyTable)
+
+  DatabaseConnector::executeSql(connection = connection, sql = sql)
+
+
+}
---
+++ OncoRegimenFinderA1/R/Utilits.R
@@ -0,0 +1,56 @@
+getPath <- function() {
+  return("inst/sql")
+}
+
+getThisPackageName <- function() {
+  return("OncoRegimenFinderA1")
+}
+
+getCohortBuild <- function(){
+  SqlRender::readSql(file.path(getPath(),
+                       "CohortBuild.sql"))
+}
+
+getRegimenTable <- function(){
+  SqlRender::readSql(file.path(getPath(),
+                               "RegimenTable.sql"))
+}
+
+getRegimenTable_f <- function(){
+  SqlRender::readSql(file.path(getPath(),
+                               "RegimenTable_f.sql"))
+}
+getSampledRegimenTable <- function(){
+  SqlRender::readSql(file.path(getPath(),
+                               "SampledRegimenTable.sql"))
+}
+
+getRegimenCalculation <- function(){
+  SqlRender::readSql(file.path(getPath(),
+                               "RegimenCalculation.sql"))
+}
+
+getInsertIntoRegimenTable_f <- function(){
+  SqlRender::readSql(file.path(getPath(),
+                               "InsertIntoRegimenTable_f.sql"))
+}
+
+getRawEvents <- function(){
+  SqlRender::readSql(file.path(getPath(),
+                               "RawEvents.sql"))
+}
+
+getRegimenFormat <- function(){
+  SqlRender::readSql(file.path(getPath(),
+                               "RegimenFormat.sql"))
+}
+
+getInsertIntoSampledRegimenTable <- function(){
+  SqlRender::readSql(file.path(getPath(),
+                               "InsertIntoSampledRegimenTable.sql"))
+}
+
+getRegimenVocabulary <- function(){
+  SqlRender::readSql(file.path(getPath(),
+                               "RegimenVocabulary.sql"))
+}
