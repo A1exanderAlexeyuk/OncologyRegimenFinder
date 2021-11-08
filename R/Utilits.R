f9671306abd057d3f4920eba72c0b3cc4ffcec1f
alexander alexeuyk <87428750+A1exanderAlexeyuk@users.noreply.github.com>
2021-11-08 16:08:13 +0300

    updated funcs

---
+++ R/Utilits.R
@@ -0,0 +1,69 @@
+getPath <- function() {
+  if(connectionDetails$dbms == "postgresql"){
+    return("inst/sql/Postgres")
+    }
+  if(connectionDetails$dbms == "redshift"){
+    return("inst/sql/Redshift")
+  }
+  if(connectionDetails$dbms == "bigquery"){
+    return("inst/sql/BigQuery")
+  }
+}
+
+getThisPackageName <- function() {
+  return("OncoRegimenFinderA1")
+}
+
+getCohortBuild <- function(){
+  SqlRender::readSql(file.path(getPath(),
+                       "CohortBuild.sql"))
+}
+
+getRegimenTable <- function(){
+  SqlRender::readSql(file.path(getPath(),
+                               "RegimenTable.sql"))
+}
+
+getRegimenTable_f <- function(){
+  SqlRender::readSql(file.path(getPath(),
+                               "RegimenTable_f.sql"))
+}
+getSampledRegimenTable <- function(){
+  SqlRender::readSql(file.path(getPath(),
+                               "SampledRegimenTable.sql"))
+}
+
+getRegimenCalculation <- function(){
+  SqlRender::readSql(file.path(getPath(),
+                               "RegimenCalculation.sql"))
+}
+
+getInsertIntoRegimenTable_f <- function(){
+  SqlRender::readSql(file.path(getPath(),
+                               "InsertIntoRegimenTable_f.sql"))
+}
+
+getRawEvents <- function(){
+  SqlRender::readSql(file.path(getPath(),
+                               "RawEvents.sql"))
+}
+
+getRegimenFormat <- function(){
+  SqlRender::readSql(file.path(getPath(),
+                               "RegimenFormat.sql"))
+}
+
+getInsertIntoSampledRegimenTable <- function(){
+  SqlRender::readSql(file.path(getPath(),
+                               "InsertIntoSampledRegimenTable.sql"))
+}
+
+getRegimenVocabulary <- function(){
+  SqlRender::readSql(file.path(getPath(),
+                               "RegimenVocabulary.sql"))
+}
+
+getRegimenFormatWithoutVocabulary <- function(){
+  SqlRender::readSql(file.path(getPath(),
+                               "RegimenFormatWithoutVocabulary.sql"))
+}
