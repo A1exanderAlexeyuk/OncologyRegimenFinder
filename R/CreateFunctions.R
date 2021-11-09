ab22b0f9fcb973f8c8b359734b6176ab8e2720bd
alexander alexeuyk <87428750+A1exanderAlexeyuk@users.noreply.github.com>
2021-11-09 10:38:24 +0300

    updated sql deliver and remove regimen repeats (from adam)

--- R/CreateFunctions.R
+++ R/CreateFunctions.R
@@ -5,7 +5,7 @@ createCohortTable <- function(connection,
                               regimenTable,
                               drugClassificationIdInput){
 
-  sql <- SqlRender::render(sql = getCohortBuild(),
+  sql <- SqlRender::render(sql = readDbSql("CohortBuild.sql", connection@dbms),
                            cdmDatabaseSchema = cdmDatabaseSchema,
                            writeDatabaseSchema = writeDatabaseSchema,
                            cohortTable = cohortTable,
@@ -15,11 +15,11 @@ createCohortTable <- function(connection,
   DatabaseConnector::executeSql(connection = connection, sql = sql)
 }
 
-createSapmledRegimenTable <- function(connection,
+createSampledRegimenTable <- function(connection,
                                       writeDatabaseSchema,
                                       regimenTable,
                                       sampleSize){
-  sqlTemp <- SqlRender::render("SELECT max(rn) FROM @writeDatabaseSchema.@regimenTable;",
+  sqlTemp <- SqlRender::render("SELECT max(rn) as max FROM @writeDatabaseSchema.@regimenTable;",
                                writeDatabaseSchema = writeDatabaseSchema,
                                regimenTable = regimenTable)
 
@@ -27,7 +27,7 @@ createSapmledRegimenTable <- function(connection,
   message(paste0("Cohort contains ", maxId$max, " subjects"))
   idGroups <- c(seq(1, maxId$max, sampleSize), maxId$max + 1)
 
-  sql <- SqlRender::render(getRegimenTable_f(),
+  sql <- SqlRender::render(readDbSql("RegimenTable_f.sql", connection@dbms),
                            regimenTable_f = paste0(regimenTable,"_f"),
                            writeDatabaseSchema = writeDatabaseSchema)
 
@@ -38,7 +38,7 @@ createSapmledRegimenTable <- function(connection,
     endId = idGroups[g+1] - 1
 
     message(paste0("Processing persons ",startId," to ",endId))
-    sql <- SqlRender::render(getInsertIntoSampledRegimenTable(),
+    sql <- SqlRender::render(readDbSql("InsertIntoSampledRegimenTable.sql", connection@dbms),
                              writeDatabaseSchema = writeDatabaseSchema,
                              regimenTable = regimenTable,
                              sampledRegimenTable = paste0(regimenTable,"_sampled"),
@@ -52,24 +52,20 @@ createSapmledRegimenTable <- function(connection,
 createRegimenCalculation <- function(connection,
                                      writeDatabaseSchema,
                                      regimenTable,
-                                     dateLagInput,
-                                     regimenRepeats){
-  sql <- SqlRender::render(sql = getRegimenCalculation(),
+                                     dateLagInput
+                                    ){
+  sql <- SqlRender::render(sql = readDbSql("RegimenCalculation.sql", connection@dbms),
                            writeDatabaseSchema = writeDatabaseSchema,
                            regimenTable = regimenTable,
                            dateLagInput= dateLagInput)
 
-  for(i in c(1:regimenRepeats))
-  {
-    DatabaseConnector::executeSql(connection = connection, sql = sql)
   }
-}
 
 
 InsertIntoRegimenTable_f <- function(connection,
                                      writeDatabaseSchema,
                                      regimenTable){
-  sql <- SqlRender::render(getInsertIntoRegimenTable_f(),
+  sql <- SqlRender::render(readDbSql("InsertIntoRegimenTable_f.sql", connection@dbms),
                            writeDatabaseSchema = writeDatabaseSchema,
                            sampledRegimenTable = paste0(regimenTable,"_sampled"),
                            regimenTable_f = paste0(regimenTable,"_f"))
@@ -86,16 +82,15 @@ createRawEvents <- function(connection,
                             dateLagInput,
                             generateRawEvents){
   if(generateRawEvents){
-  sql <- render(sql = getRawEvents(),
-                rawEventTable = rawEventTable,
-                cancerConceptId = cancerConceptId,
-                writeDatabaseSchema = cohortDatabaseSchema,
-                cdmDatabaseSchema = cdmDatabaseSchema,
-                drugClassificationIdInput = drugClassificationIdInput,
-                dateLagInput = dateLagInput
-  )
-
-  executeSql(connection = connection, sql = sql)
+    sql <- render(sql = readDbSql("RawEvents.sql"),
+                  rawEventTable = rawEventTable,
+                  cancerConceptId = cancerConceptId,
+                  writeDatabaseSchema = cohortDatabaseSchema,
+                  cdmDatabaseSchema = cdmDatabaseSchema,
+                  drugClassificationIdInput = drugClassificationIdInput,
+                  dateLagInput = dateLagInput)
+  
+    executeSql(connection = connection, sql = sql)
   }
 }
 
@@ -104,16 +99,13 @@ createVocabulary <- function(connection,
                              cdmDatabaseSchema,
                              vocabularyTable,
                              generateVocabTable){
-  if(generateVocabTable){
+  
+  sql <- SqlRender::render(sql = readDbSql("RegimenVocabulary.sql", connection@dbms),
+                           writeDatabaseSchema = writeDatabaseSchema,
+                           cdmDatabaseSchema = cdmDatabaseSchema,
+                           vocabularyTable = vocabularyTable)
 
-    sql <- SqlRender::render(sql = getRegimenVocabulary(),
-                             writeDatabaseSchema = writeDatabaseSchema,
-                             cdmDatabaseSchema = cdmDatabaseSchema,
-                             vocabularyTable = vocabularyTable)
-
-    DatabaseConnector::executeSql(connection = connection, sql = sql)
-
-  }
+  DatabaseConnector::executeSql(connection = connection, sql = sql)
 }
 
 
@@ -125,9 +117,9 @@ createRegimenFormatTable <- function(connection,
                                      vocabularyTable,
                                      generateVocabTable){
   if(generateVocabTable){
-    sql_t <- getRegimenFormat()
-    }else{
-      sql_t <- getRegimenFormatWithoutVocabulary()
+      sql_t <- readDbSql("RegimenFormat.sql", connection@dbms)
+    } else {
+      sql_t <- readDbSql("RegimenFormatWithoutVocabulary.sql", connection@dbms)
     }
   try(sql <- SqlRender::render(sql = sql_t,
                            writeDatabaseSchema = writeDatabaseSchema,
