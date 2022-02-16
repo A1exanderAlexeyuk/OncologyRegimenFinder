8ed627f4f06262362c23c1cef93d11401b3860af
alexander alexeuyk <87428750+A1exanderAlexeyuk@users.noreply.github.com>
2022-02-16 17:38:47 +0300

    Delete inst/sql/sql_server directory

--- inst/sql/sql_server/distributions/RemoveComplementaryTables.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS @cohortDatabaseSchema.subject_age;
-
-DROP TABLE IF EXISTS @cohortDatabaseSchema.charlson_concepts;
-
-DROP TABLE IF EXISTS @cohortDatabaseSchema.charlson_scoring;
-
-DROP TABLE IF EXISTS @cohortDatabaseSchema.charlson_map;
